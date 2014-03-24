531ba31b718270d312074203a8e2aa2c9670607e
nickmeinhold <nickmeinhold@gmail.com>
2014-03-24 13:52:44 +1100

    Initial Commit

---
+++ Hypnosister/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Hypnosister
+//
+//  Created by Nick Meinhold on 24/03/2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hypnosister/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hypnosister
+//
+//  Created by Nick Meinhold on 24/03/2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
