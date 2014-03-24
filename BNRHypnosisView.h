93ddde6b4e9dff2d8bdb7ad94d552e52cadcddd3
nickmeinhold <nickmeinhold@gmail.com>
2014-03-24 16:18:39 +1100

    Initial Commit. Created a view that draws circles and added an image.

---
+++ BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Nick Meinhold on 24/03/2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
--- Hypnosister/BNRAppDelegate.m
+++ Hypnosister/BNRAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "BNRAppDelegate.h"
+#import "BNRHypnosisView.h"
 
 @implementation BNRAppDelegate
 
@@ -14,6 +15,12 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    CGRect firstFrame = self.window.bounds;
+    
+    BNRHypnosisView *firstView = [[BNRHypnosisView alloc] initWithFrame:firstFrame];
+    [self.window addSubview:firstView];
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
